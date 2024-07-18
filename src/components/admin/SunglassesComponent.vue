b7fc5d1ab4cebbbf0f92b140bad6fdbad524a843
Aqeel <aqeelhanslo@gmail.com>
2024-07-18 16:49:07 +0200

    Created admin sunglass component with delete and update info funtions

--- src/components/admin/SunglassesComponent.vue
+++ src/components/admin/SunglassesComponent.vue
@@ -1,5 +1,119 @@
 <template>
-  <div class="row"></div>
+  <div class="row q-pa-md q-gutter-md justify-center">
+
+    <!-- ALL SUNGLASSES CARD -->
+    <q-card class="q-pa-md col-12 col-md-9">
+      <q-card-section>
+        <div class="text-h6">All Sunglasses</div>
+      </q-card-section>
+      <q-markup-table>
+        <thead>
+          <tr>
+            <th></th>
+            <th class="text-left">Model</th>
+            <th class="text-left">Description</th>
+            <th class="text-left">Price</th>
+            <th class="text-left">Stock</th>
+            <th class="text-left">Tools</th>
+          </tr>
+        </thead>
+        <tbody v-for="(sunglass, index) in sunglasses" :key="sunglass._id">
+          <tr>
+            <td class="text-left cursor-pointer">{{ index + 1 }}</td>
+            <td class="text-left cursor-pointer">
+              <q-input v-if="editMode === sunglass._id" v-model="sunglass.model" />
+              <div v-else>
+                {{ sunglass.model }}
+              </div>
+            </td>
+            <td class="text-left cursor-pointer">
+              <q-input v-if="editMode === sunglass._id" v-model="sunglass.description" />
+              <div v-else class="limit-text">
+                {{ sunglass.description }}
+              </div>
+            </td>
+            <td class="text-left cursor-pointer">
+              <q-input v-if="editMode === sunglass._id" v-model="sunglass.price" />
+              <div v-else>
+                {{ sunglass.price }}
+              </div>
+            </td>
+            <td class="text-left cursor-pointer">
+              <q-input v-if="editMode === sunglass._id" v-model="sunglass.stock" />
+              <div v-else>
+                {{ sunglass.stock }}
+              </div>
+            </td>
+            <td class="text-left cursor-pointer">
+              <q-btn dense flat label="Delete" color="negative" @click="deleteSunglasses(sunglass)" />
+              <q-btn dense flat label="Edit" color="primary" v-if="editMode !== sunglass._id" @click="editMode = sunglass._id" />
+              <q-btn dense flat label="Update" color="primary"  v-if="editMode === sunglass._id" @click="updateSunglasses(sunglass)" />
+              <q-btn dense flat label="Cancel" color="secondary" v-if="editMode === sunglass._id" @click="editMode = null" />
+            </td>
+          </tr>
+        </tbody>
+      </q-markup-table>
+    </q-card>
+  </div>
 </template>
 
-<script></script>
+<script>
+import SunglassesService from 'src/services/SunglassesService'
+
+export default {
+  data() {
+    return {
+      sunglasses: [{}],
+      editMode: null,
+    }
+  },
+  methods: {
+    async getAllSunglasses() {
+      const response = await SunglassesService.findAllSunglasses()
+      this.sunglasses = response
+    },
+    // updates only details - no images
+    async updateSunglasses(sunglasses) {
+      const updatedSunglasses = {
+        model: sunglasses.model,
+        description: sunglasses.description,
+        color: sunglasses.color,
+        price: sunglasses.price,
+        stock: sunglasses.stock,
+        images: this.sunglasses.images
+      }
+      this.$q.dialog({
+        title: 'Update sunglasses',
+        message: `You are about to update ${sunglasses.model}, continue?`,
+        cancel: true,
+        persistent: true
+      }).onOk(async () => {
+        try {
+          await SunglassesService.updateSunglasses(sunglasses._id, updatedSunglasses)
+          this.editMode = null
+        } catch (error) {
+          console.error(error);
+        }
+      }).onCancel(() => {}).onDismiss(() => {});
+    },
+    async deleteSunglasses(sunglasses) {
+      this.$q.dialog({
+        title: 'Delete sunglasses',
+        message: `You are about to delete ${sunglasses.model}, continue?`,
+        cancel: true,
+        persistent: true
+      }).onOk(async () => {
+        try {
+            await SunglassesService.deleteSunglasses(sunglasses._id)
+            await this.getAllSunglasses()
+          } catch (error) {
+            console.error(error);
+          }
+      }).onCancel(() => {}).onDismiss(() => {});
+    },
+  },
+  created() {
+    this.getAllSunglasses()
+  }
+}
+</script>
--- src/components/admin/UserComponent.vue
+++ src/components/admin/UserComponent.vue
@@ -3,7 +3,7 @@
   <div class="row q-pa-md q-gutter-md justify-center">
 
     <!-- =================================== USER STAT CARD -->
-    <q-card class="q-pa-md col-12 col-md-5">
+    <q-card class="q-pa-sm col-12 col-md-5">
       <q-card-section>
         <div class="text-h6">Frequent Users Statistics</div>
       </q-card-section>
@@ -12,86 +12,92 @@
       </q-card-section>
     </q-card>
 
-    <!-- =================================== USER DETAILS CARD -->
-    <q-card v-if="selectedUser" class="q-pa-md col-12 col-md-6">
-      <q-card-section>
-        <div class="text-h6">{{ selectedUser.username }} Details</div>
-      </q-card-section>
-      <q-markup-table>
-        <tbody>
-          <tr>
-            <td class="text-left cursor-pointer">First Name: </td>
-            <td class="text-left cursor-pointer">{{ selectedUser.firstName }}</td>
-          </tr>
-          <tr>
-            <td class="text-left cursor-pointer">Last Name: </td>
-            <td class="text-left cursor-pointer">{{ selectedUser.lastName }}</td>
-          </tr>
-          <tr>
-            <td class="text-left cursor-pointer">Email: </td>
-            <td class="text-left cursor-pointer">{{ selectedUser.email }}</td>
-          </tr>
-          <tr>
-            <td class="text-left cursor-pointer">Phone: </td>
-            <td class="text-left cursor-pointer">{{ selectedUser.phone }}</td>
-          </tr>
-          <tr>
-            <td class="text-left cursor-pointer">User Type: </td>
-            <td class="text-left cursor-pointer">
-              <q-select v-if="editMode.userType" v-model="selectedUser.userType" :options="userTypeOptions" @blur="updateUserType(selectedUser)" emit-value map-options />
-              <div v-else @click="editMode.userType = true">
-                {{ selectedUser.userType }}
-                <q-icon name="eva-edit-outline" style="font-size: 16px; opacity: 75%;" class="q-ml-sm q-mb-xs" />
-              </div>
-            </td>
-          </tr>
-          <tr></tr>
-        </tbody>
-      </q-markup-table>
-      <!-- close button -->
-      <div class="row justify-start">
-        <q-card-actions>
-          <q-btn flat label="Close" color="primary" @click="closeDetails" />
-        </q-card-actions>
-        <q-card-actions>
-          <q-btn flat label="Delete" color="negative" @click="deleteUser(selectedUser._id, selectedUser.username)" />
-        </q-card-actions>
-        <q-card-actions>
-          <q-btn flat label="Logout" color="warning" v-if="selectedUser.loginInfo && selectedUser.loginInfo.isLoggedIn === true " @click="logoutUser(selectedUser._id, selectedUser.username)" />
-        </q-card-actions>
-      </div>
-    </q-card>
-
     <!-- =================================== ALL USERS CARD -->
-    <q-card class="q-pa-md col-12 col-md-6">
-      <q-card-section>
-        <div class="text-h6">All Users</div>
-      </q-card-section>
-      <q-markup-table>
-        <thead>
-          <tr>
-            <th class="text-left">Username</th>
-            <th class="text-left">First Name</th>
-            <th class="text-left">Last Name</th>
-            <th class="text-left">Date Last Login</th>
-            <th class="text-left">Login Status</th>
-          </tr>
-        </thead>
-        <tbody v-for="user in users" :key="user._id">
-          <tr @click="openDetails(user)" :class="{ 'admin-user': user.userType === 'admin' }">
-            <td class="text-left cursor-pointer">{{ user.username }}</td>
-            <td class="text-left cursor-pointer">{{ user.firstName }}</td>
-            <td class="text-left cursor-pointer">{{ user.lastName }}</td>
-            <td class="text-left cursor-pointer">
-              {{ user.loginInfo && user.loginInfo.lastLogin ? new Date(user.loginInfo.lastLogin).toLocaleDateString('en-GB') : '' }}
-            </td>
-            <td class="text-center cursor-pointer">
-              <span v-if="user.loginInfo && user.loginInfo.isLoggedIn" class="green-dot"></span>
-              <span v-else class="red-dot"></span>
-            </td>
-          </tr>
-        </tbody>
-      </q-markup-table>
+    <q-card class="q-pa-sm col-12 col-md-6">
+
+      <!-- details -->
+      <q-card v-if="selectedUser">
+        <q-card-section>
+          <div class="text-h6">{{ selectedUser.username }} Details</div>
+        </q-card-section>
+        <q-markup-table>
+          <tbody>
+            <tr>
+              <td class="text-left cursor-pointer">First Name: </td>
+              <td class="text-left cursor-pointer">{{ selectedUser.firstName }}</td>
+            </tr>
+            <tr>
+              <td class="text-left cursor-pointer">Last Name: </td>
+              <td class="text-left cursor-pointer">{{ selectedUser.lastName }}</td>
+            </tr>
+            <tr>
+              <td class="text-left cursor-pointer">Email: </td>
+              <td class="text-left cursor-pointer">{{ selectedUser.email }}</td>
+            </tr>
+            <tr>
+              <td class="text-left cursor-pointer">Phone: </td>
+              <td class="text-left cursor-pointer">{{ selectedUser.phone }}</td>
+            </tr>
+            <tr>
+              <td class="text-left cursor-pointer">User Type: </td>
+              <td class="text-left cursor-pointer">
+                <q-select v-if="editMode.userType" v-model="selectedUser.userType" :options="userTypeOptions" @blur="updateUserType(selectedUser)" emit-value map-options />
+                <div v-else @click="editMode.userType = true">
+                  {{ selectedUser.userType }}
+                  <q-icon name="eva-edit-outline" style="font-size: 16px; opacity: 75%;" class="q-ml-sm q-mb-xs" />
+                </div>
+              </td>
+            </tr>
+            <tr></tr>
+          </tbody>
+        </q-markup-table>
+        <!-- close button -->
+        <div class="row justify-start">
+          <q-card-actions>
+            <q-btn dense flat label="Close" color="primary" @click="closeDetails" />
+          </q-card-actions>
+          <q-card-actions>
+            <q-btn dense flat label="Delete" color="negative" @click="deleteUser(selectedUser._id, selectedUser.username)" />
+          </q-card-actions>
+          <q-card-actions>
+            <q-btn dense flat label="Logout" color="warning" v-if="selectedUser.loginInfo && selectedUser.loginInfo.isLoggedIn === true " @click="logoutUser(selectedUser._id, selectedUser.username)" />
+          </q-card-actions>
+        </div>
+      </q-card>
+
+      <!-- all users -->
+      <q-card flat v-else>
+        <q-card-section>
+          <div class="text-h6">All Users</div>
+        </q-card-section>
+        <q-markup-table>
+          <thead>
+            <tr>
+              <td></td>
+              <th class="text-left">Username</th>
+              <th class="text-left">First Name</th>
+              <th class="text-left">Last Name</th>
+              <th class="text-left">Date Last Login</th>
+              <th class="text-left">Login Status</th>
+            </tr>
+          </thead>
+          <tbody v-for="(user, index) in users" :key="user._id">
+            <tr @click="openDetails(user)" :class="{ 'admin-user': user.userType === 'admin' }">
+              <td class="text-left cursor-pointer">{{ index + 1 }}</td>
+              <td class="text-left cursor-pointer">{{ user.username }}</td>
+              <td class="text-left cursor-pointer">{{ user.firstName }}</td>
+              <td class="text-left cursor-pointer">{{ user.lastName }}</td>
+              <td class="text-left cursor-pointer">
+                {{ user.loginInfo && user.loginInfo.lastLogin ? new Date(user.loginInfo.lastLogin).toLocaleDateString('en-GB') : '' }}
+              </td>
+              <td class="text-center cursor-pointer">
+                <span v-if="user.loginInfo && user.loginInfo.isLoggedIn" class="green-dot"></span>
+                <span v-else class="red-dot"></span>
+              </td>
+            </tr>
+          </tbody>
+        </q-markup-table>
+      </q-card>
     </q-card>
   </div>
 </template>
@@ -166,7 +172,9 @@ export default {
       this.users = response
     },
     async updateUserType(selectedUser) {
+      const currentUser = await UserService.FindUserByToken()
       const userId = this.selectedUser._id
+
       const updatedUser = {
         firstName: this.selectedUser.firstName,
         lastName: this.selectedUser.lastName,
@@ -180,13 +188,36 @@ export default {
         loginInfo: this.selectedUser.loginInfo,
         order: this.selectedUser.order
       }
-      const response = await UserService.updateUserDetails(userId, updatedUser)
-      console.log(response)
-      // window.location.reload()
+
+      if (currentUser._id === userId) {
+        this.$q.dialog({
+            title: 'Error',
+            message: 'You cannot change your usertype!',
+            ok: 'OK'
+          }).onOk(() => {
+            window.location.reload();
+            return;
+          });
+          return;
+      } else {
+        this.$q.dialog({
+          title: 'Update user',
+          message: `You are about to update ${updatedUser.username}, continue?`,
+          cancel: true,
+          persistent: true
+        }).onOk(async () => {
+          try {
+            await UserService.updateUserDetails(userId, updatedUser)
+            // window.location.reload();
+          } catch (error) {
+            console.error(error);
+          }
+        }).onCancel(() => {}).onDismiss(() => {});
+      }
     },
     async deleteUser(userId, username) {
-      const admin = await UserService.FindUserByToken()
-      if (admin._id === userId) {
+      const currentUser = await UserService.FindUserByToken()
+      if (currentUser._id === userId) {
         this.$q.dialog({
             title: 'Error',
             message: 'You cannot delete yourself!',
@@ -208,13 +239,12 @@ export default {
           } catch (error) {
             console.error(error);
           }
-        })
-        .onCancel(() => {}).onDismiss(() => {});
+        }).onCancel(() => {}).onDismiss(() => {});
       }
     },
     async logoutUser(userId, username) {
-      const admin = await UserService.FindUserByToken()
-      if (admin._id === userId) {
+      const currentUser = await UserService.FindUserByToken()
+      if (currentUser._id === userId) {
         this.$q.dialog({
             title: 'Error',
             message: 'You are currently logged in!',
