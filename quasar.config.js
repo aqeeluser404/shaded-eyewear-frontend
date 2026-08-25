69efea63b7c1dd41d4cc87b69d68fcf8c7c09d6e
Aqeel <aqeelhanslo@gmail.com>
2026-08-25 15:01:30 +0200

    update vueRouterMode to history in quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -58,7 +58,7 @@ export default configure(function (/* ctx */) {
         node: 'node20'
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
